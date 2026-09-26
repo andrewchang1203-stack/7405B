a68352dfafad24a77e6217dcdd932a748529e970
Cduck360 <145716083+Cduck360@users.noreply.github.com>
2026-09-25 20:16:00 -0400

    big code stuff
    new auto

--- src/autons.cpp
+++ src/autons.cpp
@@ -5,7 +5,14 @@ void test() {
     // set position to x:0, y:0, heading:0
     chassis.setPose(0, 0, 0);
     // turn to face heading 90 with a very long timeout
-    chassis.turnToHeading(90, 3000);
-    //chassis.moveToPoint(0, 10, 5000);
+    //chassis.turnToHeading(90, 3000);
+    cascade.move_absolute(720, 127);
+    pros::delay(200);
+    chassis.moveToPoint(0, 5, 500);
+    chassis.moveToPoint(0, -20, 1000, {.forwards = false, .minSpeed = 127});
+    pros::delay(500);
+    chassis.moveToPoint(0,8.3,1000);
+    chassis.turnToHeading(90,1000);
+    
     // chassis.moveToPoint(0, 30, 1000);
 }
--- src/devices.cpp
+++ src/devices.cpp
@@ -3,8 +3,8 @@
 #include "main.h"
 #include "lift.h"
 
-pros::MotorGroup left_motor_group({-10, -16}, pros::MotorGears::blue);
-pros::MotorGroup right_motor_group({7, 19}, pros::MotorGears::blue);
+pros::MotorGroup left_motor_group({-7, -19}, pros::MotorGears::blue);
+pros::MotorGroup right_motor_group({10, 16}, pros::MotorGears::blue);
 
 //pros::MotorGroup cascade({[0]=-2, [1]=11} gearset::pros:MotorGears::blue); // cascade lift motors
 
@@ -14,7 +14,7 @@ pros::MotorGroup cascade {-2,11};
 lemlib::Drivetrain drivetrain(&left_motor_group, // left motor group
                               &right_motor_group, // right motor group
                               10.9, //track width
-                              lemlib::Omniwheel::NEW_325,
+                              lemlib::Omniwheel::NEW_275,
                               450, // drivetrain rpm is 360
                               1 // horizontal drift is 2 (for now)
 );
@@ -22,9 +22,9 @@ lemlib::Drivetrain drivetrain(&left_motor_group, // left motor group
 // imu
 pros::Imu imu(3);
 
-pros::Rotation horizontal_rotation(-17);
+pros::Rotation horizontal_rotation(17);
 
-lemlib::TrackingWheel horizontal_tracking_wheel(&horizontal_rotation, lemlib::Omniwheel::NEW_2, -1.8);
+lemlib::TrackingWheel horizontal_tracking_wheel(&horizontal_rotation, lemlib::Omniwheel::NEW_2, 1.8);
 
 lemlib::OdomSensors sensors(nullptr, // vertical tracking wheel 1, set to null
                             nullptr, // vertical tracking wheel 2, set to nullptr as we are using IMEs
@@ -44,9 +44,9 @@ lemlib::OdomSensors sensors(nullptr, // vertical tracking wheel 1, set to null
 //                                               500, // large error range timeout, in milliseconds
 //                                               20 // maximum acceleration (slew)
 // );
-lemlib::ControllerSettings lateral_controller(10, // proportional gain (kP)
+lemlib::ControllerSettings lateral_controller(6, // proportional gain (kP)
                                               0, // integral gain (kI)
-                                              3, // derivative gain (kD)
+                                              5, // derivative gain (kD)
                                               3, // anti windup
                                               1, // small error range, in inches
                                               100, // small error range timeout, in milliseconds
@@ -68,7 +68,7 @@ lemlib::ControllerSettings lateral_controller(10, // proportional gain (kP)
 // );
 lemlib::ControllerSettings angular_controller(2, // proportional gain (kP)
                                               0, // integral gain (kI)
-                                              0, // derivative gain (kD)
+                                              13.5, // derivative gain (kD)
                                               3, // anti windup
                                               1, // small error range, in inches
                                               100, // small error range timeout, in milliseconds
